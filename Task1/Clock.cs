9f76f29a69be2466155b505e2bd9a90daddca1cb
StepanLyalkovski <stepanlyalkovski@gmail.com>
2016-03-30 18:47:27 +0300

    add Clock, add event for countdown

---
+++ Task1/Clock.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Clock
+    {
+        private TimeSpan time = new TimeSpan();
+        public event EventHandler<EventArgs> TimeIsOver;
+
+        protected virtual void OnTimeIsOver(object sender, EventArgs e)
+        {
+            TimeIsOver?.Invoke(sender, e);
+        }
+
+        public void SetCountDown(int seconds)
+        {
+            time = TimeSpan.FromSeconds(seconds);
+        }
+
+        public void TurnOn()
+        {
+            Thread thread = new Thread(Ticks);
+            thread.Start();
+        }
+
+        public void Ticks()
+        {
+            double timeRemaining = time.TotalSeconds;
+
+            while (timeRemaining > 0)
+            {
+                timeRemaining--;
+                Thread.Sleep(1000);
+                //Debug.WriteLine(timeRemaning);
+            }
+            OnTimeIsOver(this, EventArgs.Empty);
+        }
+
+    }
+}
