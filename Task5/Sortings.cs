f14079231415de2e66137c50dbed248e0149900b
StepanLyalkovski <stepanlyalkovski@gmail.com>
2016-03-31 17:39:39 +0300

    add Bubble sort

---
+++ Task5/Sortings.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    public static class Sortings
+    {
+
+        public static void Bubble<T>(T[] array)
+        {
+            Bubble(array, null);
+        }
+
+        public static void Bubble<T>(T[] array, IComparer<T> comparer)
+        {
+            IComparer<T> comp = comparer ?? Comparer<T>.Default;
+            for (int write = 0; write < array.Length; write++)
+            {
+                for (int sort = 0; sort < array.Length - 1; sort++)
+                {                    
+                    if (comp.Compare(array[sort], array[sort + 1]) > 0)
+                    {
+                        T temp = array[sort + 1];
+                        array[sort + 1] = array[sort];
+                        array[sort] = temp;
+                    }
+                }
+            }
+
+        }
+
+    }
+}
