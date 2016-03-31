0b958e58c657d6b5de88221af3a608c5238fd394
StepanLyalkovski <stepanlyalkovski@gmail.com>
2016-03-31 12:08:52 +0300

    add Fibonacci sequence

---
+++ Task2/Fibonacci.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class Fibonacci
+    {
+        public static IEnumerable<int> GetSequence(int number)
+        {
+            int num1 = 0;
+            int num2 = 1;
+
+            for (int i = 0; i < number; i++)
+            {
+                yield return num1;
+                int temp = num2;
+                num2 += num1;
+                num1 = temp;
+            }
+        }
+    }
+}
